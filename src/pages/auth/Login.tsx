407a4772f7cfb285d03ae0c4547e5816acec04dc
CuonggTV <rinjin2004@gmail.com>
2025-11-30 10:25:45 +0700

    fix: login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,22 +1,33 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [email, setEmail] = useState<string>("");
     const [pwd, setPwd] = useState<string>("");
+    const navigate = useNavigate();
 
     const handleLogin = () => {
         console.log("Login....")
-    }   
+    }
 
     return (
-        <div>
-            <h2>Login</h2>
+        <Box sx={{ width: 300, margin: "50px auto" }}>
+            <Typography variant="h4" mb={2}> Login </Typography>
             <form onSubmit={handleLogin}>
-                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" value={pwd} onChange={e => setPwd(e.target.value)} />
+                <TextField label="Email" type="email" placeholder="Email" variant="outlined" fullWidth value={email} onChange={e => setEmail(e.target.value)} margin="normal" />
+                <TextField label="Password" type="password" placeholder="Password" variant="outlined" fullWidth value={pwd} onChange={e => setPwd(e.target.value)} margin="normal" />
+
+                <Button type="submit" variant="contained" fullWidth sx={{ mt: 2 }}>
+                    Login
+                </Button>
+
+                <Button variant="text" fullWidth sx={{ mt: 1 }} onClick={() => navigate("/register")}>
+                    Create an account
+                </Button>
                 <button type="submit">Login</button>
             </form>
-        </div>
+        </Box>
     )
 }
 
