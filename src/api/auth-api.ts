d2f4700814a3c04506af862eada4e7a4938ce5ad
CuonggTV <rinjin2004@gmail.com>
2025-12-01 11:20:56 +0700

    remove unused import

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,12 +1,8 @@
 import type { UserDto } from "../types/user.dto";
-import api from "./axios";
-import { API_ENDPOINTS } from "./endpoints";
 
 export default class PrivateAPI {
 
     static async getUserByToken(): Promise<UserDto> {
-        // const axiosResposne = await api.get("https://localhost:5000" + API_ENDPOINTS.ACCOUNT.GET);
-        // const response: ResponseDto<GetUserResponseDTO> = axiosResposne.data;
 
         // fake delay để giả lập async call
         await new Promise((resolve) => setTimeout(resolve, 500));
