109432930a4150460911cda9054637bc6b7ba4b1
CuonggTV <rinjin2004@gmail.com>
2026-01-19 13:40:02 +0700

    ref: clean up page

--- src/routes/public-route.tsx
+++ src/routes/public-route.tsx
@@ -1,7 +1,6 @@
 import { Route } from "react-router-dom";
 import GuestGuard from "../auth/guards/GuestGuard";
 import { PAGE_ENDPOINTS } from "../api/endpoints";
-import Login from "@/pages/auth/Login";
 import MyForm from "@/pages/MyForm";
 
 const PublicRoutesComponent = () => {
