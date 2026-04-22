112ebddc369136704fbd030c7e9f70541ad88f4d
ThinhND3004 <nguyendaithinh3004@gmail.com>
2026-04-22 21:10:12 +0700

    fix model + shop list page

--- src/api/product-api.ts
+++ src/api/product-api.ts
@@ -507,7 +507,7 @@ export default class ProductAPI {
   }
 
   static async getFrameGroupModel3D(frameGroupId: string): Promise<Blob> {
-    const modelUrl = `${API_CONFIG.BASE_URL}api/v1/product/frame-group/model-3d?frameGroupId=${encodeURIComponent(frameGroupId)}`;
+    const modelUrl = `${API_CONFIG.BASE_URL}/api/v1/product/frame-group/model-3d?frameGroupId=${encodeURIComponent(frameGroupId)}`;
     const response = await axiosInstance.get(modelUrl, {
       responseType: 'blob',
     });
